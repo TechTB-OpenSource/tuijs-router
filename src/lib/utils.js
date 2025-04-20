641b4c4768bfb6add256da38616cf0049d792e11
jhdelar <98446060+jhdelar@users.noreply.github.com>
2025-04-20 14:59:50 -0400

    road_v0.6.0-016

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -24,6 +24,7 @@ export function matchRoute(testRoutePath, sanitizedTargetRoute) {
         const urlParams = {};
         const urlObj = new URL(sanitizedTargetRoute, window.location.origin);
         const queryParams = Object.fromEntries(urlObj.searchParams.entries());
+        console.log(queryParams)
         const testRouteParts = testRoutePath.split('/');
         const targetRouteParts = sanitizedTargetRoute.split('/');
         // Check if the path part counts are different. If so, then the routes don't match.
@@ -48,8 +49,8 @@ export function matchRoute(testRoutePath, sanitizedTargetRoute) {
         }
 
         // If the test route passes all tests, true is returned.
-        const combinedParams = { ...urlParams, ...queryParams };
-        return { matches: true, combinedParams };
+        const params = { ...urlParams, ...queryParams };
+        return { matches: true, params };
     } catch (er) {
         console.error(`TUI Router Error: utils > matchRoute`);
         console.error(er);
