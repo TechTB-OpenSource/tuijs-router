558a0c8e4b960517a7660f6679a205569a90fc0c
jhdelar <98446060+jhdelar@users.noreply.github.com>
2025-07-24 14:18:36 -0400

    v0.7.4
    - Added type exports

--- src/index.js
+++ src/index.js
@@ -25,6 +25,22 @@ import {
     navigateToAnchorTag
 } from './lib/navigate.js';
 
+/**
+ * @typedef {import('./types.js').Route} Route
+ * @typedef {import('./types.js').RouteList} RouteList
+ * @typedef {import('./types.js').RouteNotFound} RouteNotFound
+ * @typedef {import('./types.js').ServerRouteList} ServerRouteList
+ * @typedef {import('./types.js').Redirect} Redirect
+ * @typedef {import('./types.js').RedirectList} RedirectList
+ * @typedef {import('./types.js').RouterConfig} RouterConfig
+ * @typedef {import('./types.js').ActiveRoute} ActiveRoute
+ */
+
+/**
+ * Creates a router instance
+ * @returns {Object} Router instance with all methods
+ */
+
 function createRouter() {
     return {
         startRouter,
@@ -54,3 +70,15 @@ function createRouter() {
 
 export { createRouter };
 export { createRouter as tuiRouter };
+
+// Export types for TypeScript consumers
+export {
+    Route,
+    RouteList,
+    RouteNotFound,
+    ServerRouteList,
+    Redirect,
+    RedirectList,
+    RouterConfig,
+    ActiveRoute
+} from './types.js';
--- src/types.js
+++ src/types.js
@@ -1,31 +1,8 @@
 /**
- * Data Example
- * 
- * route Object Example:
- *      {
- *          path: '/',
- *          enterFunction: functionName,
- *          exitFunction: functionName,
- *      }
- * 
- * routeNotFound Object Example:
- *      {
- *          server: false,
- *          path: '/404'
- *      }
- * 
- * redirect Object Example:
- *      {
- *          fromPath: '/old-path',
- *          toPath:  '/new-path'
- *      }
- */
-
-/**
  * @typedef {Object} Route
- * @property {string} path - The route path (e.g., '/about')
- * @property {Function} handlerFunction - Function to run when the route matches
- * @property {Function|null} [exitFunction] - Optional function to run when exiting the route
+ * @property {string} path
+ * @property {Function} enterFunction
+ * @property {Function|null} [exitFunction]
  */
 
 /**
@@ -34,19 +11,18 @@
 
 /**
  * @typedef {Object} RouteNotFound
- * @property {boolean} server - A boolean showing if a route should be forwarded to the server or SPA router.
- * @property {string} path - The path to send route not found requests to.
+ * @property {boolean} server
+ * @property {string} path
  */
 
 /**
- * @typedef {Array} ServerRouteList
- * @property {string} - A route/path which will always be directed to the server.
+ * @typedef {string[]} ServerRouteList
  */
 
 /**
  * @typedef {Object} Redirect
- * @property {string} fromPath - The route being hit that will trigger a redirect.
- * @property {string} toPath - The target path of the redirect once triggered.
+ * @property {string} fromPath
+ * @property {string} toPath
  */
 
 /**
@@ -55,13 +31,24 @@
 
 /**
  * @typedef {Object} RouterConfig
- * @property {RouteList} routeList - List of route definitions
- * @property {ServerRouteList} serverRouteList - List of path redirects
- * @property {RouteNotFound} routeNotFound - Handler for unknown routes
- * @property {RedirectList} redirectList - List of path redirects
+ * @property {RouteList} routeList
+ * @property {ServerRouteList} serverRouteList
+ * @property {RouteNotFound} routeNotFound
+ * @property {RedirectList} redirectList
  */
 
 /**
  * @typedef {Object} ActiveRoute
- * @property {object} route - An active route
+ * @property {Route} route
  */
+
+export {
+    Route,
+    RouteList,
+    RouteNotFound,
+    ServerRouteList,
+    Redirect,
+    RedirectList,
+    RouterConfig,
+    ActiveRoute
+}
